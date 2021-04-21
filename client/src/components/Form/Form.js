635646b51ed73ff338f1c3b566298ec5eaa8d1c5
Midstackdev <midstackdev@gmail.com>
2021-04-21 20:36:55 +0000

    Update a post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { createPost } from '../../actions/posts'
+import { createPost, updatePost } from '../../actions/posts'
 import useStyles from './styles'
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -15,24 +15,45 @@ const Form = () => {
         selectedFile: '',
     })
 
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null )
+
     const classes = useStyles()
 
     const dispatch = useDispatch()
 
+    useEffect(() => {
+        if(post) setPostData(post)
+    }, [post])
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        dispatch(createPost(postData))
+        if(currentId) {
+
+            dispatch(updatePost(currentId, postData))
+            
+        }else {
+
+            dispatch(createPost(postData))
+        }
+        clear()
     }
 
     const clear = () => {
-
+        setCurrentId(null)
+        setPostData({
+            creator: '',
+            title: '',
+            message: '',
+            tags: '',
+            selectedFile: '',
+        })
     }
 
     return (
         <Paper className={classes.paper}>
            <form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate onSubmit={handleSubmit}>
-               <Typography variant="h6">Creating a Memory</Typography>
+               <Typography variant="h6">{currentId ? 'Editing': 'Creating'} a Memory</Typography>
                <TextField
                 name="creator"
                 variant="outlined"
