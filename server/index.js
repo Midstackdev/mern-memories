062a577683307ca127de8c63a60d4cc4bcb2cffe
Midstackdev <midstackdev@gmail.com>
2021-04-21 23:03:10 +0000

    constants and env

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/post.js'
 
 const app = express()
-
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -14,8 +15,8 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = `mongodb+srv://modstacks:fYv4LqCvHohMISGN@cluster0.yuaby.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-const PORT = process.env.PORT || 5000 
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT  
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true,
