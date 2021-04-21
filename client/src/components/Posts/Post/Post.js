aba3cbebc2230c897d7ef95733e8ff9a367794dc
Midstackdev <midstackdev@gmail.com>
2021-04-21 21:01:37 +0000

    elete post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,10 +5,15 @@ import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 import moment from 'moment'
 
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
+
 import useStyles from './styles'
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -34,7 +39,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button siz="small" color="primary" onClick={() => {}}>
+                <Button siz="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"></DeleteIcon>
                     Delete
                 </Button>
