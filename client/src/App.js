79e89ef96654b979201165c17478dcc115a75ca3
Midstackdev <midstackdev@gmail.com>
2021-04-21 13:31:19 +0000

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import react from 'react'
+import react, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
 
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
@@ -9,6 +11,11 @@ import useStyles from './styles'
 
 const App = () => {
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   return (
     <Container maxWidth="lg">
