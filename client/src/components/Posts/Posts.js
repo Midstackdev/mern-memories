9858e177ad7b37cf79e0f883296d9293a04b6d05
Midstackdev <midstackdev@gmail.com>
2021-04-21 19:09:36 +0000

    Show posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { CircularProgress, Grid} from '@material-ui/core'
 
 import Post from './Post/Post'
 import useStyles from './styles'
@@ -10,11 +11,17 @@ const Posts = () => {
 
     console.log(posts)
     return (
-        <>
-            <h1>Posts</h1>
-            <Post></Post>
-            <Post></Post>
-        </>
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6}>
+                            <Post post={post} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     )
 }
 
